33af0eb698ff7f0d0776ff0ff33a876bd3872319
Joshuah Solito <jsolito@morbius.mscs.mu.edu>
2019-04-04 21:29:05 -0500

    readded xinu-hw8

---
+++ xinu-hw8/system/free.c
@@ -0,0 +1,33 @@
+// TA-BOT:MAILTO joshuah.solito@marquette.edu akshay.verma@marquette.edu
+/**
+ * @file free.c
+ */
+/* Embedded Xinu, Copyright (C) 2009, 2013.  All rights reserved. */
+
+#include <xinu.h>
+
+/**
+ * @ingroup libxc
+ *
+ * Attempt to free a block of memory based on malloc() accounting information
+ * stored in preceding two words.
+ *
+ * @param ptr
+ *      A pointer to the memory block to free.
+ */
+syscall free(void *ptr)
+{
+    struct memblock *block;
+
+	/* TODO:
+     *      1) set block to point to memblock to be free'd (ptr)
+     *      2) find accounting information of the memblock
+     *      3) call freemem on the block with its length
+     */
+
+	block = (struct memblock *)ptr;
+	freemem(ptr, block->length);// call freemem on the block with its length, 
+	//block pointing to length (block->length) will be used to find accounting info
+	
+    return OK;
+}
---
+++ xinu-hw8/system/signal.c
@@ -0,0 +1,56 @@
+//TA-BOT:MAILTO joshuah.solito@marquette.edu akshay.verma@marquette.edu
+/**
+ * @file signal.c
+ *
+ */
+/* Embedded Xinu, Copyright (C) 2009.  All rights reserved. */
+
+#include <xinu.h>
+
+/**
+ * @ingroup semaphores
+ *
+ * Signal a semaphore, releasing up to one waiting thread.
+ *
+ * signal() may reschedule the currently running thread.  As a result, signal()
+ * should not be called from non-reentrant interrupt handlers unless ::resdefer
+ * is set to a positive value at the start of the interrupt handler.
+ *
+ * @param sem
+ *      Semaphore to signal.
+ *
+ * @return
+ *      ::OK on success, ::SYSERR on failure.  This function can only fail if @p
+ *      sem did not specify a valid semaphore.
+ */
+syscall signal(semaphore sem)
+{
+    register struct sement *semptr;
+    int count;
+    irqmask im;
+
+    im = disable();
+    if (isbadsem(sem))
+    {
+        restore(im);
+        return SYSERR;
+    }
+
+    semptr = &semtab[sem];
+
+    /* TODO:
+     * Make this incrementation atomically-safe
+     * so that multiple cores may safely signal a semaphore.
+     */
+	count = _atomic_increment(&semptr->count);
+
+    if (count <= 0)
+    {
+        ready(dequeue(semptr->queue), RESCHED_NO, getcpuid());
+    }
+
+    udelay(3);
+    restore(im);
+
+    return OK;
+}
