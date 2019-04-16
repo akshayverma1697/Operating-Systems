d9d7c6334e94e9906a4ad290e095af96dcfc86eb
Akshay Verma <averma13@argolis.mscsnet.mu.edu>
2019-04-15 23:20:14 -0500

    first attempt

--- xinu-hw9/file/fileDelete.c
+++ xinu-hw9/file/fileDelete.c
@@ -21,5 +21,26 @@ devcall fileDelete(int fd)
     //  and return its space to the free disk block list.
     //  Use the superblock's locks to guarantee mutually exclusive
     //  access to the directory index.
+    
+    if((NULL == supertab) || (NULL == filetab)
+        || (isbadfd(fd)) || (FILE_FREE == filetab[fd].fn_state))
+    {
+        return SYSERR;
+    }
+    
+    
+    if(FILE_DIRTY & filetab[fd].fn_state)
+    {
+        seek(DISK0, filetab[fd].fn_blocknum);
+        write(DISK0, filetab[fd].fn_data, DISKBLOCKLEN);
+        filetab[fd].fn_state &= ~FILE_DIRTY;
+    }
+    filetab[fd].fn_state &= ~FILE_OPEN;
+    
+    wait(supertab->sb_freelock);
+    seek(DISK0, supertab->sb_dirlst->db_blocknum);
+    signal(supertab->sb_dirlock);
+    
+    
     return OK;
 }
--- xinu-hw9/file/sbFreeBlock.c
+++ xinu-hw9/file/sbFreeBlock.c
@@ -23,24 +23,62 @@ devcall sbFreeBlock(struct superblock *psuper, int block)
     //  free blocks.  Use the superblock's locks to guarantee
     //  mutually exclusive access to the free list, and write
     //  the changed free list segment(s) back to disk.
-    struct freeblock *freeblk;
+    struct freeblock *freeblk, *free2;
+    struct dirblock *swizzle;
+    struct dentry *phw;
+    struct disk *dsk;
+
+ 
+    if(NULL == psuper)
+    {
+        return SYSERR;
+    }
     
-    diskfd = phw - ftab;
-    freeblk = psupe->sb_freelst;
+    phw = psuper->sb_disk;
     
+    if(NULL == phw)
+    {
+        return SYSERR;
+    }
     
-    wait(sem freeblock);
+    //diskfd = phw - ftab;
+    freeblk = psuper->sb_freelst;
     
-    //first conditon add if there is enough space
+    if(NULL == freeblk)
+    {
+        printf("Free List is empty\n");
+        return SYSERR;
+    }
+    
+    //guarantee mutually exclusive access to freelist
+    wait(psuper->sb_freelock);
+    
+    while(freeblk->fr_next != NULL)
+    {
+        freeblk = freeblk->fr_next;//traverse freelist until it reaches the end++++
+    }
+    //first conditon see if there is enough space
     if(freeblk->fr_count < FREEBLOCKMAX)
     {
         freeblk->fr_count++;
+        freeblk->fr_free[freeblk->fr_count] = block;//puts block back into list?????
+        
+        //free2 
     }
         
-        
+     /*   
     //if it is greater than or equal to
-        
+    if(freeblk->fr_count >= FREEBLOCKMAX)//after 55 blocks have been allocated
+    {
+        fr_count = 0;//free all space (nuke the freelist)???
+        freeblk->fr_free[freeblk->fr_count] = block;//use all that space for block????
+    }
     
     //if supertab->sb_freelst = null
+    if(psuper->sb_freelst == NULL)//after all blocks have been used
+    {
+        psuper->sb_freelst = freeblk;//I DONT KNOW WHAT TO DO
+    }
+    */
     return SYSERR;
 }
