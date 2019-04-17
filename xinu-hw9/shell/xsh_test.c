debb100168de9ae8113e850c3121d5f964c65de6
Akshay Verma <averma13@morbius.mscs.mu.edu>
2019-04-16 20:31:54 -0500

    fuck

--- xinu-hw9/shell/xsh_test.c
+++ xinu-hw9/shell/xsh_test.c
@@ -12,9 +12,40 @@
  * @param args array of arguments
  * @return OK for success, SYSERR for syntax error
  */
+void printFreeList1(void);
 command xsh_test(int nargs, char *args[])
 {
     //TODO: Test your O/S.
     printf("This is where you should put some testing code.\n");
     return OK;
 }
+
+
+void printFreeList1(void)
+{
+    int i;
+    struct freeblock *fb = NULL;
+    if ((NULL == supertab) || (NULL == (fb = supertab->sb_freelst)))
+    {
+        printf("Freelst: null\n");
+        return;
+    }
+
+    printf("Freelst collector nodes:\n");
+    while (fb)
+    {
+        printf("Blk %3d, cnt %3d = ", fb->fr_blocknum, fb->fr_count);
+        for (i = 0; (i < fb->fr_count) && (i < 10); i++)
+        {
+            printf("[%03d]", fb->fr_free[i]);
+        }
+        if (fb->fr_count >= 10)
+        {
+            printf("...[%03d]", fb->fr_free[fb->fr_count - 1]);
+        }
+        printf("\n");
+        fb = fb->fr_next;
+    }
+}
+
+
